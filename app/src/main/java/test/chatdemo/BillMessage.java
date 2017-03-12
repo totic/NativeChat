2c12aabaec4d2dce68cf4387b45400d245484485
totic <federico.mora@gmail.com>
2017-03-12 10:57:46 -0400

    Added basic object to hold bill data

--- app/src/main/java/test/chatdemo/BillMessage.java
+++ app/src/main/java/test/chatdemo/BillMessage.java
@@ -1,20 +1,50 @@
 package test.chatdemo;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class BillMessage {
 
     private String content;
-    private boolean isMine;
+    private String mAccountNumber;
+    private BigDecimal mProductPriceAmount;
+    private BigDecimal mTaxesAmount;
+    private BigDecimal mTotalAmount;
+    private Date mDueDate;
 
-    public BillMessage(String content, boolean isMine) {
-        this.content = content;
-        this.isMine = isMine;
-    }
 
     public String getContent() {
         return content;
     }
 
-    public boolean isMine() {
-        return isMine;
+    public BillMessage(String content, String accountNumber, BigDecimal productPriceAmount,
+                       BigDecimal taxesAmount, BigDecimal totalAmount, Date dueDate) {
+
+        this.content = content;
+        this.mAccountNumber = accountNumber;
+        this.mProductPriceAmount = productPriceAmount;
+        this.mTaxesAmount = taxesAmount;
+        this.mTotalAmount = totalAmount;
+        this.mDueDate = dueDate;
+    }
+
+    public String getAccountNumber() {
+        return mAccountNumber;
+    }
+
+    public BigDecimal getProductPriceAmount() {
+        return mProductPriceAmount;
+    }
+
+    public BigDecimal getTaxesAmount() {
+        return mTaxesAmount;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return mTotalAmount;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
     }
 }
