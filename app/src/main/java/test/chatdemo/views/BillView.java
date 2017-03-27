976d8b432404d6ffcd430074afd71beb94fb0377
totic <federico.mora@gmail.com>
2017-03-27 14:09:23 -0400

    Added animation to bill and stylised bubles

--- app/src/main/java/test/chatdemo/views/BillView.java
+++ app/src/main/java/test/chatdemo/views/BillView.java
@@ -1,12 +1,24 @@
 package test.chatdemo.views;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.Transformation;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.lang.reflect.Method;
 
 import test.chatdemo.R;
 
@@ -22,6 +34,10 @@ public class BillView extends LinearLayout {
     private TextView taxes;
     private TextView total;
     private TextView dueDate;
+    private LinearLayout fullLayout;
+    private LinearLayout topLayout;
+    private LinearLayout bottomLayout;
+    private FrameLayout frameLayout;
 
     public BillView(Context context) {
         super(context);
@@ -40,6 +56,10 @@ public class BillView extends LinearLayout {
         this.taxes =          (TextView)rootView.findViewById(R.id.taxes);
         this.dueDate =        (TextView)rootView.findViewById(R.id.due_date);
         this.total =          (TextView)rootView.findViewById(R.id.total);
+        this.frameLayout =    (FrameLayout)rootView.findViewById(R.id.frame_bill);
+        this.fullLayout=      (LinearLayout)rootView.findViewById(R.id.all_bill);
+        this.topLayout =      (LinearLayout)rootView.findViewById(R.id.top_bill);
+        this.bottomLayout=    (LinearLayout)rootView.findViewById(R.id.bottom_bill);
     }
 
     public void setAccountNumber(String accountNumber) {
@@ -60,5 +80,57 @@ public class BillView extends LinearLayout {
 
     public void setDueDate(String dueDate) {
         this.dueDate.setText(dueDate);
+    }
+
+    public void animateBill(final int fullWidth){
+        this.measure(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+        final int fullHeight = BillView.this.frameLayout.getMeasuredHeight();
+        frameLayout.getLayoutParams().height = fullHeight;
+        frameLayout.getLayoutParams().width = fullWidth;
+
+        BillView.this.fullLayout.getLayoutParams().height = 0;
+        BillView.this.fullLayout.getLayoutParams().width = 0;
+        BillView.this.fullLayout.setVisibility(VISIBLE);
+
+        ValueAnimator va = ValueAnimator.ofFloat(0, 100);
+        va.setDuration(getResources().getInteger(R.integer.animation_bill_start));
+        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Float value = (Float) animation.getAnimatedValue();
+                float scale = value.floatValue()/100;
+                if(BillView.this.getLayoutParams()!=null){
+                    BillView.this.fullLayout.getLayoutParams().height = (int)(scale * fullHeight);
+                    BillView.this.fullLayout.getLayoutParams().width =  (int)(scale * fullWidth);
+                }
+                BillView.this.fullLayout.requestLayout();
+            }
+        });
+        va.addListener(new AnimatorListenerAdapter(){
+            @Override
+            public void onAnimationEnd(Animator animation){
+                BillView.this.topLayout.setVisibility(VISIBLE);
+                AlphaAnimation anim = new AlphaAnimation(0,1);
+                anim.setDuration(getResources().getInteger(R.integer.animation_bill_mid));
+                BillView.this.topLayout.startAnimation(anim);
+                anim.setAnimationListener(new Animation.AnimationListener() {
+                    public void onAnimationStart(Animation a) {}
+                    public void onAnimationRepeat(Animation a) {}
+                    public void onAnimationEnd(Animation a) {
+                        AlphaAnimation anim = new AlphaAnimation(0,1);
+                        BillView.this.bottomLayout.setVisibility(VISIBLE);
+                        anim.setDuration(getResources().getInteger(R.integer.animation_bill_end));
+                        BillView.this.bottomLayout.startAnimation(anim);
+                    }
+                });
+            }
+        });
+        va.start();
+    }
+
+    public void displayBill(){
+        this.fullLayout.setVisibility(VISIBLE);
+        this.topLayout.setVisibility(VISIBLE);
+        this.bottomLayout.setVisibility(VISIBLE);
     }
 }
