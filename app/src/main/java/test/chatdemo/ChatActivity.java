74aa660aaf89e66bcbd82fd402c08d383ecc229f
totic <federico.mora@gmail.com>
2017-03-27 01:06:33 -0400

    Improved matching for word bill

--- app/src/main/java/test/chatdemo/ChatActivity.java
+++ app/src/main/java/test/chatdemo/ChatActivity.java
@@ -24,6 +24,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +35,8 @@ public class ChatActivity extends Activity {
     private MessageAdapter adapter;
     private DBMessageHandler handler;
     private Handler msghandler = new Handler();
+
+    private final Pattern patternBill = Pattern.compile(".*\\bbill(s?)\\b.*",Pattern.DOTALL|Pattern.CASE_INSENSITIVE);
 
     @BindView(R.id.list_msg) ListView listView;
     @BindView(R.id.btn_chat_send) Button btnSend;
@@ -45,40 +49,36 @@ public class ChatActivity extends Activity {
         ButterKnife.bind(this);
 
         handler = new DBMessageHandler(this);
-        Cursor cursor = handler.getCursor();
-
-        adapter = new MessageAdapter(this, cursor);
+        adapter = new MessageAdapter(this, handler.getCursor());
         listView.setAdapter(adapter);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText.getText().toString().trim().equals("")) {
-                    Toast.makeText(ChatActivity.this, "Please input some text...", Toast.LENGTH_SHORT).show();
+                if (editText.getText().toString().trim().length()==0) {
+                    Toast.makeText(ChatActivity.this, R.string.input_text, Toast.LENGTH_SHORT).show();
                 } else {
                     String msg = editText.getText().toString();
-
-                    if(!msg.contains("bill")){
-                        handler.addMessage(new ChatMessage(msg, MessageType.OUTGOING.getValue(), new Date()));
+                    Matcher m = patternBill.matcher(msg);
+                    if(!m.find()){
+                        handler.addMessage(new ChatMessage(msg, MessageType.OUTGOING.getValue()));
                         sendIncomingMsg(msg);
                     }else{
-                        handler.addMessage(new ChatMessage(msg, MessageType.BILL.getValue(), new Date()));
+                        handler.addMessage(new ChatMessage(msg, MessageType.OUTGOING.getValue()));
+                        handler.addMessage(new ChatMessage(msg, MessageType.BILL.getValue()));
                     }
-                    Cursor cursor = handler.getCursor();
-                    adapter.swapCursor(cursor);
+                    adapter.swapCursor(handler.getCursor());
                     adapter.notifyDataSetChanged();
-
                     editText.setText("");
                 }
             }
         });
-
     }
 
     public void sendIncomingMsgWithDelay(final String msg) {
         Runnable runnable = new Runnable() {
             public void run() {
-                handler.addMessage(new ChatMessage(msg + " -- A", MessageType.INCOMING.getValue(), new Date()));
+                handler.addMessage(new ChatMessage(msg, MessageType.INCOMING.getValue(), new Date()));
                 Cursor cursor = handler.getCursor();
                 adapter.swapCursor(cursor);
                 adapter.notifyDataSetChanged();
@@ -88,6 +88,6 @@ public class ChatActivity extends Activity {
     }
 
     public void sendIncomingMsg(final String msg) {
-        handler.addMessage(new ChatMessage(msg + " -- A", MessageType.INCOMING.getValue(), new Date()));
+        handler.addMessage(new ChatMessage(msg, MessageType.INCOMING.getValue(), new Date()));
     }
 }
