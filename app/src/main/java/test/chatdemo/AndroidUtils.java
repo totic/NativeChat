331ac5d7105faf76b03acc9babe8ddfbb8f49310
totic <federico.mora@gmail.com>
2017-03-27 14:07:50 -0400

    cleanup

--- app/src/main/java/test/chatdemo/AndroidUtils.java
+++ app/src/main/java/test/chatdemo/AndroidUtils.java
@@ -17,17 +17,7 @@ import java.util.Date;
 
 public class AndroidUtils {
 
-    public static void changeAndroidColor(Activity activity, int statusbarColor){
-        Window window = activity.getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColor(ContextCompat.getColor(activity, statusbarColor));
-        }
-    }
-
     private static final SimpleDateFormat allDay = new SimpleDateFormat("MM/dd/yyyy");
-
     private static final SimpleDateFormat fullDate = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
     private static final SimpleDateFormat dayDate  = new SimpleDateFormat("MM/dd hh:mm a");
     private static final SimpleDateFormat time     = new SimpleDateFormat("hh:mm a");
